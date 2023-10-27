7e693ae75c1f9509b4417100c2e16a6cfcb5bcef
harshil09 <harshil.t@tridhyatech.com>
2023-10-27 12:38:56 +0530

    Add Base Fragment

---
+++ app/src/main/java/com/tridhya/basesetupnew/base/BaseFragment.kt
@@ -0,0 +1,52 @@
+package com.tridhya.basesetupnew.base
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.tridhya.basesetupnew.R
+
+open class BaseFragment : Fragment(){
+    lateinit var mContext: Context
+    lateinit var mActivity: Activity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        mContext = context
+        mActivity = context as Activity
+    }
+
+    fun setupToolbarWithMenu(view: View, title: String? = null, icon: Int = R.drawable.v_ic_menu) {
+        val toolbar = view.findViewById<Toolbar>(R.id.toolbar)
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        val actionBar = (activity as AppCompatActivity).supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(false)
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(icon)
+            val tvTitle = toolbar.findViewById<TextView>(R.id.tvTitle)
+            if (title != null) tvTitle.text = title
+        }
+        toolbar.setNavigationOnClickListener {
+            //(activity as MainActivity).openDrawer()
+        }
+    }
+
+    @JvmOverloads
+    fun setUpToolbarWithBackArrow(strTitle: String? = null, isBackArrow: Boolean = true) {
+        val toolbar = view?.findViewById<Toolbar>(R.id.toolbar)
+        val title = toolbar?.findViewById<TextView>(R.id.tvTitle)
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        val actionBar = (activity as AppCompatActivity).supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayShowTitleEnabled(false)
+            actionBar.setDisplayHomeAsUpEnabled(isBackArrow)
+            actionBar.setHomeAsUpIndicator(R.drawable.v_ic_back_arrow)
+            if (strTitle != null) title?.text = strTitle
+        }
+    }
+}
